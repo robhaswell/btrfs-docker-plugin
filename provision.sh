8e4b2172836a9d7c82e5ec95fd6ebec08476fcf3
Rob Haswell <me@robhaswell.co.uk>
2015-08-19 17:10:20 +0100

    Setup.py

--- provision.sh
+++ provision.sh
@@ -1,8 +1,15 @@
 sudo apt-get update
 sudo apt-get -y install git btrfs-tools
 
-git clone https://github.com/robhaswell/btrfs-docker-plugin.git
 sudo mkfs.btrfs /dev/sdb
 sudo mkdir /mnt/btrfs
 sudo mount -t btrfs /dev/sdb /mnt/btrfs
 sudo mkdir /mnt/docker
+
+curl -sSL https://get.docker.com/ | sudo sh
+
+git clone https://github.com/robhaswell/btrfs-docker-plugin.git
+
+cd btrfs-docker-plugin
+
+sudo python setup.py install
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name="BTRFS Docker Plugin",
+    version="0.1",
+    author="Rob Haswell",
+    author_email="rob@clusterhq.com",
+    url="https://github.com/robhaswell/btrfs-docker-plugin",
+    license="Apache License, Version 2.0",
+    packages=[
+        "btrfs_docker",
+    ],
+    entry_points={
+        "console_scripts": [
+            "btrfs-docker-plugin = btrfs_docker.plugin:main",
+        ],
+    },
+    install_requires=[
+        "flask",
+    ],
+)
