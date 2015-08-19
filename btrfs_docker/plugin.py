429920117ddd2002fab08cef079205ba60d1cc21
Rob Haswell <me@robhaswell.co.uk>
2015-08-19 18:01:58 +0100

    Make mountpoint first.

--- btrfs_docker/plugin.py
+++ btrfs_docker/plugin.py
@@ -10,20 +10,19 @@ def plugin_activate():
 @app.route('/VolumeDriver.Create', methods=['POST'])
 def volume_create():
     data = json.loads(request.get_data())
+    os.system("mkdir -p /mnt/docker/%s" % (data['Name'],))
     os.system("btrfs subvolume create /mnt/btrfs/%s" % (data['Name'],))
     return '{"Err": null}'
 
 @app.route('/VolumeDriver.Mount', methods=['POST'])
 def volume_mount():
     data = json.loads(request.get_data())
-    os.system("mkdir -p /mnt/docker/%s" % (data['Name'],))
     os.system("mount -t btrfs -o subvol=%s /dev/sdb /mnt/docker/%s" % (data['Name'], data['Name']))
     return '{"Mountpoint": "/mnt/docker/%s", "Err": null}' % (data['Name'],)
 
 @app.route('/VolumeDriver.Path', methods=['POST'])
 def volume_path():
     data = json.loads(request.get_data())
-    os.system("mount -t btrfs -o subvol=%s /dev/sdb /mnt/docker/%s" % (data['Name'], data['Name']))
     return '{"Mountpoint": "/mnt/docker/%s", "Err": null}' % (data['Name'],)
 
 @app.route('/VolumeDriver.Unmount', methods=['POST'])
